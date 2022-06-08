f3d6cb6ebc089ef4c0baca2adf4f690761f90fdc
Lolloc99 <lolloc99@yahoo.it>
2022-06-08 15:23:16 +0200

    Snack 1 completato

--- Snack-1/index.php
+++ Snack-1/index.php
@@ -12,5 +12,44 @@
 Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema.
 Olimpia Milano - Cantù | 55-60
 -->
+
+<?php 
+$matches = [
+    [
+        "homeTeam" => "Hyperion",
+        "guestTeam" => "Maliwan",
+        "ptHomeTeam" => 47,
+        "ptGuestTeam" => 60,
+    ],
+    [
+        "homeTeam" => "Hyperion",
+        "guestTeam" => "Maliwan",
+        "ptHomeTeam" => 60,
+        "ptGuestTeam" => 52,
+    ],
+    [
+        "homeTeam" => "Hyperion",
+        "guestTeam" => "Maliwan",
+        "ptHomeTeam" => 60,
+        "ptGuestTeam" => 34,
+    ],
+];
+?>
+
+<ul>
+    <?php 
+    for($i = 0; $i < count($matches); $i++) {
+        $current_match = $matches[$i];
+    ?>
+        <li>
+            <h3>
+                <?php echo $matches[$i]["homeTeam"]?> - <?php echo $matches[$i]["guestTeam"]?> 
+                | 
+                <?php echo $matches[$i]["ptHomeTeam"]?>-<?php echo $matches[$i]["ptGuestTeam"]?>
+            </h3>
+        </li>
+    <?php }?>
+</ul>
+
 </body>
 </html>
